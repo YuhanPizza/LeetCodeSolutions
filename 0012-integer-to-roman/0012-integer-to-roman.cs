508ab58c1e0c994a11356c4638a092b722fd89bc
Lorenzo Alvino Caballo Tubo <107896556+YuhanPizza@users.noreply.github.com>
2023-12-06 10:51:31 -0500

    Time: 85 ms (9.40%), Space: 49.2 MB (6.77%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cs
+++ 0012-integer-to-roman/0012-integer-to-roman.cs
@@ -2,20 +2,15 @@ public class Solution {
     public string IntToRoman(int num) {
         int[] val = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
         string[] rome={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-         
-        string outPut ="";
-        int i =0;
-        while(num > 0 && i < val.Length){
-            if (num >= val[i])
-            {
-                outPut += rome[i];
-                num -= val[i];
-            }
-            else
-            {
-                i++;
+        
+        if(num > 0){
+            for (int j =0; j < val.Length; j++){
+                if(val[j] <= num){
+                    num -= val[j];
+                    return rome[j] += IntToRoman(num);
+                }  
             }
         }
-        return outPut;
+        return "";
     }
 }
