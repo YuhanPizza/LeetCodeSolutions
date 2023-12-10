3b7dc728f2b55844974b08d19a8b384c6f3e9c55
Lorenzo Alvino Caballo Tubo <107896556+YuhanPizza@users.noreply.github.com>
2023-12-09 22:19:41 -0500

    Time: 128 ms (57.62%), Space: 43.1 MB (5.45%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        int current = 0;
+        for(int i = 0; i < nums.Length; i++){
+            if(nums[i] != val){
+                nums[current] = nums[i];
+                current++;
+            }
+        }
+        return current;
+    }
+}
