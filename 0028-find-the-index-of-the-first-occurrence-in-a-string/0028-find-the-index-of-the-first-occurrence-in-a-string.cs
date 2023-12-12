cd49e2db4d47f6c665185f855e4c148d8b3c45d4
Lorenzo Alvino Caballo Tubo <107896556+YuhanPizza@users.noreply.github.com>
2023-12-12 04:52:56 -0500

    Time: 65 ms (18.13%), Space: 37.1 MB (25.88%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cs
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cs
@@ -1,8 +1,5 @@
 public class Solution {
     public int StrStr(string haystack, string needle) {
-        if(haystack.Contains(needle)){
             return haystack.IndexOf(needle);
-        }
-        return -1;
     }
 }
