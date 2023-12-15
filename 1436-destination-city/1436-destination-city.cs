750298808b2f0e7cba78ca673e97637b4d15a6ec
Lorenzo Alvino Caballo Tubo <107896556+YuhanPizza@users.noreply.github.com>
2023-12-14 23:18:06 -0500

    Time: 104 ms (58.88%), Space: 41.8 MB (48.60%) - LeetHub

--- 1436-destination-city/1436-destination-city.cs
+++ 1436-destination-city/1436-destination-city.cs
@@ -1,16 +1,17 @@
 public class Solution {
     public string DestCity(IList<IList<string>> paths) {
         HashSet<string> unique = new HashSet<string>();
+        string ans = "";
         
         foreach (var path in paths){
             unique.Add(path[0]);
         }
         foreach (var path in paths){
             if(!unique.Contains(path[1])){
-                return path[1];
+                ans = path[1];
             }
         }
         
-        return unique.Last();
+        return ans;
     }
 }
