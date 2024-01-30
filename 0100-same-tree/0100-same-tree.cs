b8c5eb790023ec1b908523acbec029f4398fb1f0
Lorenzo Alvino Caballo Tubo <107896556+YuhanPizza@users.noreply.github.com>
2024-01-30 07:40:16 -0500

    Time: 73 ms (60.15%), Space: 42.6 MB (8.49%) - LeetHub

--- 0100-same-tree/0100-same-tree.cs
+++ 0100-same-tree/0100-same-tree.cs
@@ -16,10 +16,7 @@ public class Solution {
         if(p == null && q == null){
             return true;
         }
-        if(p == null || q == null){
-            return false;
-        }
-        if(p.val != q.val){
+        if(p == null || q == null || p.val != q.val){
             return false;
         }
         return IsSameTree(p.left, q.left) && IsSameTree(p.right,q.right);
