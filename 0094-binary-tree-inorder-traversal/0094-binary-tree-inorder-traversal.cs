ce7f99f7b27936149f36454ec7945c9d9a08dbe9
Lorenzo Alvino Caballo Tubo <107896556+YuhanPizza@users.noreply.github.com>
2024-01-29 21:25:07 -0500

    Time: 101 ms (84.01%), Space: 45.6 MB (13.75%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cs
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cs
@@ -14,17 +14,15 @@
 public class Solution {
     public IList<int> InorderTraversal(TreeNode root) {
         IList<int> result = new List<int>();
-        if(root == null){
-            return result;
-        }
-        Stack<TreeNode> stack = new Stack<TreeNode>();
+        if(root == null) return result;
+        Stack<TreeNode> potato = new Stack<TreeNode>();
         TreeNode curr = root;
-        while(curr != null || stack.Count != 0){
-            while(curr != null){
-                stack.Push(curr);
+        while(curr != null || potato.Count() != 0){
+            while(curr !=null){
+                potato.Push(curr);
                 curr = curr.left;
             }
-            curr = stack.Pop();
+            curr = potato.Pop();
             result.Add(curr.val);
             curr = curr.right;
         }
